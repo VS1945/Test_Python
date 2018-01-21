2205d0644450e6ed41605c79cc7323be7e9d1036
VS1945 <35665712+VS1945@users.noreply.github.com>
2018-01-21 22:41:19 +0300

    Update function.py

--- function.py
+++ function.py
@@ -1,11 +1,11 @@
 """x for x in s1 if x in s2"""
-"""X=99
+X=99
 def func(Y):
     Z=X+Y
     return Z
-print (func(1))"""
+print (func(1))
 
-"""def maker(N):
+def maker(N):
     def action (X):
         return X**N
     return action
@@ -14,15 +14,15 @@ print(f)
 print(f(3))
 g=maker(3)
 print(g)
-print(g(3))"""
+print(g(3))
 
 
-"""def changer(a, b): 
+def changer(a, b): 
     a = 2 
     b[0] = 'spam' 
 X = 1
 L = [1, 2] 
 changer(X, L)                                               
-print (X,L)"""
+print (X,L)
 
 
